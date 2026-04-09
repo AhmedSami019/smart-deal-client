ad82dc8775b26a192513fe531545abbe4de90f4f
ahmedsami019 <easami019192@gmail.com>
2026-04-09 12:56:13 +0600

    define the home layout

---
+++ src/Layouts/HomeLayout/HomeLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <header>
+                <nav>
+                    
+                </nav>
+            </header>
+        </div>
+    );
+};
+
+export default HomeLayout;
