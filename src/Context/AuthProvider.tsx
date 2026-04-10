c205763d4c04bf59c40e8258878be2571c1d633e
ahmedsami019 <easami019192@gmail.com>
2026-04-10 12:37:11 +0600

    user authenticaton update

--- src/Context/AuthProvider.tsx
+++ src/Context/AuthProvider.tsx
@@ -1,19 +1,47 @@
-import type { ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import { AuthContext } from "./AuthContext";
-
+import auth from "../firebase/firebase.init.ts";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  type User,
+} from "firebase/auth";
 
 const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
-    const authInfo  = {
+  // use create a new user
+  const createNewUser = (email, password) => {
+    setLoading(true);
+    createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    } 
+  // to login user by
+  const loginUser = (email, password) => {
+    setLoading(true);
+    signInWithEmailAndPassword(auth, email, password);
+  };
 
-    return (
-        <AuthContext value={authInfo}>
-            {children}
-        </AuthContext>
-    );
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    createNewUser,
+    loginUser,
+  };
+
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
-
