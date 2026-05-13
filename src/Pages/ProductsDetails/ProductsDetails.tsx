9077116b0ff525da36ae30f235d6c650996efc59
ahmedsami019 <easami019192@gmail.com>
2026-05-13 08:20:01 +0600

    added modal

--- src/Pages/ProductsDetails/ProductsDetails.tsx
+++ src/Pages/ProductsDetails/ProductsDetails.tsx
@@ -1,13 +1,47 @@
+import { useRef } from "react";
 import { useLoaderData } from "react-router";
 
 const ProductsDetails = () => {
-    const product = useLoaderData()
-    console.log(product);
-    return (
+  const product = useLoaderData();
+  console.log(product);
+
+  const bidModalRef = useRef();
+  const handleBidModal = () => {
+    bidModalRef.current.showModal();
+  };
+  return (
+    <div>
+      {/* product detail section */}
+      <div>
+        <div></div>
         <div>
-            this is product details 
+          <button onClick={handleBidModal} className="btn btn-primary">
+            I want to buy this product
+          </button>
+
+          {/* Open the modal using document.getElementById('ID').showModal() method */}
+          <dialog
+            ref={bidModalRef}
+            className="modal modal-bottom sm:modal-middle"
+          >
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
         </div>
-    );
+      </div>
+      {/* bids section */}
+    </div>
+  );
 };
 
 export default ProductsDetails;
