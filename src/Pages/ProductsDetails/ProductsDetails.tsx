91c38630146745cb25c6c5e8acc0442b9524acb0
ahmedsami019 <easami019192@gmail.com>
2026-05-14 17:25:16 +0600

    added swal2, handleClose method and fix some bug

--- src/Pages/ProductsDetails/ProductsDetails.tsx
+++ src/Pages/ProductsDetails/ProductsDetails.tsx
@@ -1,41 +1,63 @@
 import { useContext, useRef } from "react";
 import { useLoaderData } from "react-router";
 import { AuthContext } from "../../Context/AuthContext";
+import Swal from "sweetalert2";
 
 const ProductsDetails = () => {
   const { user } = useContext(AuthContext);
 
   const product = useLoaderData();
-  const {_id}= product
+  const { _id } = product;
 
   const bidModalRef = useRef<HTMLDialogElement>(null);
   const handleBidModal = () => {
     bidModalRef.current?.showModal();
   };
 
-//   bid handler
-  const handleBid = e =>{
-    e.preventDefault()
+  //   bid handler
+  const handleBid = (e) => {
+    e.preventDefault();
     const form = e.target;
-    const formData = new FormData(form)
+    const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
     const newBid = {
       product: _id,
-      buyer_name : data.name,
-      buyer_email : data.email,
-      bid_price : data.bid,
-      status : "pending"
+      buyer_name: data.name,
+      buyer_email: data.email,
+      bid_price: data.bid,
+      status: "pending",
     };
     console.log(newBid);
-    fetch('http://localhost:3000/bids', {
-      method: "POST", 
+    fetch("http://localhost:3000/bids", {
+      method: "POST",
       headers: {
-        'content-type': "application/json"
+        "content-type": "application/json",
       },
-      body: JSON.stringify(newBid)
+      body: JSON.stringify(newBid),
     })
-    .then(res => res.json())
-    .then(data => console.log("after pricing bid",data))
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          bidModalRef.current?.close();
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Your bid has been placed",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+          });
+        }
+      });
+  };
+
+  const handleClose = ()=>{
+    bidModalRef.current?.close()
   }
   return (
     <div>
@@ -61,7 +83,7 @@ const ProductsDetails = () => {
                   <label className="label">Buyer Name</label>
                   <input
                     type="name"
-                    className="input"
+                    className="input w-full"
                     name="name"
                     defaultValue={user ? user.displayName : ""}
                     placeholder={!user ? "Enter name" : ""}
@@ -69,27 +91,26 @@ const ProductsDetails = () => {
                   <label className="label">Buyer Email</label>
                   <input
                     type="email"
-                    className="input"
+                    className="input w-full"
                     name="email"
-                    defaultValue={user? user.email: ""}
-                    placeholder={!user?"Enter email": ""}
+                    defaultValue={user ? user.email : ""}
+                    placeholder={!user ? "Enter email" : ""}
                   />
-                   <label className="label">Bid price</label>
+                  <label className="label">Bid price</label>
                   <input
                     type="name"
-                    className="input"
+                    className="input w-full"
                     name="bid"
                     placeholder="Place you bid pric"
                   />
-                  <button className="btn btn-neutral mt-4">Bid now</button>
+                  <div className="flex justify-between items-center mt-5 gap-5">
+                    <button className="btn btn-primary w-1/2">Bid now</button>
+                    <button onClick={handleClose} type="button" className="btn-secondary w-1/2">
+                      <span className="btn-secondary-inner">cancel</span>
+                    </button>
+                  </div>
                 </fieldset>
               </form>
-              <div className="modal-action">
-                <form method="dialog">
-                  {/* if there is a button in form, it will close the modal */}
-                  <button className="btn">Close</button>
-                </form>
-              </div>
             </div>
           </dialog>
         </div>
