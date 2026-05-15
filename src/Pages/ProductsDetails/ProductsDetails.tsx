9576b9d98de82659c4ade5ae8a53e0124977fd6c
ahmedsami019 <easami019192@gmail.com>
2026-05-15 07:02:44 +0600

    updated the ui

--- src/Pages/ProductsDetails/ProductsDetails.tsx
+++ src/Pages/ProductsDetails/ProductsDetails.tsx
@@ -63,9 +63,11 @@ const ProductsDetails = () => {
             showConfirmButton: false,
             timer: 1500,
           });
-          newBid._id = data.insertedId
-          const newBids = [newBid, ...bids].sort((a,b)=> b.bid_price - a.bid_price)
-          setBids(newBids)  
+          newBid._id = data.insertedId;
+          const newBids = [newBid, ...bids].sort(
+            (a, b) => b.bid_price - a.bid_price,
+          );
+          setBids(newBids);
         } else {
           Swal.fire({
             icon: "error",
@@ -80,12 +82,72 @@ const ProductsDetails = () => {
     bidModalRef.current?.close();
   };
   return (
-    <div className="mx-10 my-5">
+    <div className="mx-3 md:mx-10 my-5">
       {/* product detail section */}
-      <section>
-        <div></div>
-        <div>
-          <button onClick={handleBidModal} className="btn btn-primary">
+      <section className="grid grid-cols-1 md:grid-cols-3 gap-10">
+        {/* left side */}
+        <div className="md:flex md:flex-col space-y-6 ">
+          <figure>
+            <img className="rounded-xl w-full" src={product.image} alt="" />
+          </figure>
+
+          <div className="flex-1 space-y-3 bg-base-200 p-5 rounded-xl">
+            <h3 className="text-2xl font-semibold">Product Description</h3>
+            <div className="flex justify-between text-xl font-medium">
+              <h4 className="text-sm md:text-base">
+                Condition :{" "}
+                <span className="text-primary">{product.condition}</span>
+              </h4>
+              <h4 className="text-sm md:text-base">
+                Usage time :{" "}
+                <span className="text-primary">{product.usage}</span>
+              </h4>
+            </div>
+            <div className="divider -my-2"></div>
+            <p className="mt-2">{product.description}</p>
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="md:col-span-2 space-y-5">
+          <h2 className="text-4xl font-bold">{product.title}</h2>
+          <p className="btn btn-sm rounded-2xl border-pink-700 text-pink-500">
+            {product.category}
+          </p>
+          <br />
+          <div className="bg-base-200 p-5 rounded-xl">
+            <h5 className="text-2xl font-bold">
+              ${product.price_min} - {product.price_max}
+            </h5>
+            <p>Price starts from</p>
+          </div>
+
+          {/* product details */}
+          <div className="bg-base-200 p-5 rounded-xl">
+            <h6 className="text-xl font-semibold mb-5">Product Details</h6>
+            <p>Product id : {product._id}</p>
+            <p>Posted : {product.created_at}</p>
+          </div>
+
+          {/* seller info */}
+          <div className="bg-base-200 p-5 rounded-xl">
+            <h6 className="text-xl font-semibold mb-5">Seller Information</h6>
+            <div className="flex items-center gap-5 mb-3">
+              <div className="avatar">
+                <div className="ring-primary ring-offset-base-100 w-14 rounded-full ring-2 ring-offset-2">
+                  <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
+                </div>
+              </div>
+              <div>
+                <p>{product.seller_name}</p>
+                <p>{product.email}</p>
+              </div>
+            </div>
+            <p>Location : {product.location}</p>
+            <p>Contact : {product.seller_contact}</p>
+            <p>Status <span>{product.status}</span></p>
+          </div>
+          <button onClick={handleBidModal} className="btn btn-primary w-full">
             I want to buy this product
           </button>
 
@@ -139,10 +201,11 @@ const ProductsDetails = () => {
           </dialog>
         </div>
       </section>
+
       {/* bids section */}
       <section className="mt-10">
         <h2 className="text-3xl font-semibold">
-          Bids for this product{" "}: {" "}
+          Bids for this product :{" "}
           <span className="text-primary">{bids.length}</span>
         </h2>
         <div className="my-5">
