7258ff2aa565370d580204d0c12912e478a8ab78
ahmedsami019 <easami019192@gmail.com>
2026-05-15 23:08:41 +0600

    private route added

--- src/Context/PrivateRoute/PrivateRoute.tsx
+++ src/Context/PrivateRoute/PrivateRoute.tsx
@@ -1,12 +1,21 @@
 import { useContext } from "react";
-import { useLocation } from "react-router";
+import { Navigate, useLocation } from "react-router";
 import { AuthContext } from "../AuthContext";
+import Loading from "../../Pages/Loading/Loading";
 
 const PrivateRoute = ({children}) => {
     const location = useLocation()
 
     const {user, loading} = useContext(AuthContext)
-    if(loading)
+    if(loading){
+        return <Loading></Loading>
+    }
+
+    if(user && user.email){
+        return children
+    }
+
+    return <Navigate to={'/login'} state={location.pathname}></Navigate>
 };
 
 export default PrivateRoute;
